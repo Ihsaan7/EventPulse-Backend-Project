e04bada94bc7b90e9308b60c7330b27c8ddef6f1
Ihsaan7 <ihsaan2215@gmail.com>
2026-09-17 20:34:59 +0500

    refactor database connection and schema initialization logic

--- src/db/index.js
+++ src/db/index.js
@@ -1,37 +1,28 @@
-import sqlite3 from "sqlite3"
-import path from "path"
-import { DB_NAME } from "../constants.js"
+import sqlite3 from "sqlite3";
+import path from "path";
+import { DB_NAME } from "../constants.js";
+import { initSchema } from "./schema.js";
 
+const dbPath = path.resolve(process.cwd(), DB_NAME);
 
-const dbPath = path.resolve(process.cwd() , DB_NAME)
+const connectDB = () => {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(dbPath, async (err) => {
+      if (err) {
+        console.error("❌ SQLite connection failed:", err.message);
+        reject(err);
+      }
+      try {
+        console.log(`\n⚙️ SQLite connected successfully! File: ${dbPath}`);
 
+        await initSchema(db);
 
-const connectDB =()=>
-    {
-        return new  Promise((resolve , reject)=>
-            {
-                const db = new sqlite3.Database(dbPath ,  async(err)=>
-                    {
-                        if(err)
-                            {
-                                console.error("❌ SQLite connection failed:", err.message)
-                                reject(err)
-                            }
-                      try {
-  console.log(`\n⚙️ SQLite connected successfully! File: ${dbPath}`);
-  
-  // Initialize tables
-  await initSchema(db);
+        resolve(db);
+      } catch (schemaErr) {
+        reject(schemaErr);
+      }
+    });
+  });
+};
 
-  // Resolve connection
-  resolve(db);
-} catch (schemaErr) {
-  // Handle schema initialization error
-  reject(schemaErr);
-}
-
-                    })
-            })
-    }
-
-export default connectDB
+export default connectDB;
--- src/db/schema.js
+++ src/db/schema.js
@@ -1,57 +1,61 @@
 const schema= `
+    PRAGMA foreign_keys = ON;
+
     CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password_hash TEXT NOT NULL,
-        role TEXT CHECK(role IN('ATTENDEE' , 'ORGANIZER' , 'ADMIN')) DEFUALT 'ATTENDEE',
+        role TEXT CHECK(role IN('ATTENDEE' , 'ORGANIZER' , 'ADMIN')) DEFAULT 'ATTENDEE',
         refresh_token TEXT,
-        created_at DATETIME DEFUALT CURRENT_TIMESTAMP
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        updated_at TEXT DEFAULT CURRENT_TIMESTAMP
     );
 
     CREATE TABLE IF NOT EXISTS events(
-        id INTEGERS PRIMARY KEY INCREMENT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         organizer_id INTEGER NOT NULL,
         title TEXT NOT NULL,
-        description TEXT NOT NULL,
+        description TEXT,
         venue TEXT NOT NULL,
-        start_time  DATATIME NOT NULL,
-        status TEXT CHECK(status IN ('DRAFT','PUBLISHED','CANCELLED')) DEFAULT 'DRAFT'
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FORIENGN KEY (organizer_id) REFRENECE users(id) ON DELETE CASCADE 
+        start_time  TEXT NOT NULL,
+        status TEXT CHECK(status IN ('DRAFT','PUBLISHED','CANCELLED','COMPLETED')) DEFAULT 'DRAFT',
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (organizer_id) REFERENCES users(id) ON DELETE CASCADE 
     );
 
-    CREATE TABLE IF NO EXISTS ticket_tiers(
+    CREATE TABLE IF NOT EXISTS ticket_tiers(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         event_id INTEGER NOT NULL,
         tier_name TEXT NOT NULL,
         price REAL NOT NULL CHECK (price >=0),
         available_seats INTEGER NOT NULL,
-        FOREIGN KEY (event_id) REFRENCE events(id) ON DELETE CASCADE
-    )
+        FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
+    );
 
-    CREATE TABLE IF NO EXISTS bookings(
+    CREATE TABLE IF NOT EXISTS bookings(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         attendee_id INTEGER NOT NULL,
         tier_id INTEGER NOT NULL,
         quantity INTEGER NOT NULL,
         total_price REAL NOT NULL,
-        booking_status TEXT CHECK (booking_status IN ('PENDING_LOCK' , 'PAID' , 'CANCELLED' , 'EXPIRED')) DEFUALT 'PENDING_LOCK',
-        seat_lock_expires_at DATETIME;
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (attendee_id) REFRENCE users(id)
-        FOREIGN KEY (tier_id) REFRENCE ticket_tiers(id)
+        booking_status TEXT CHECK (booking_status IN ('PENDING_LOCK' , 'PAID' , 'CANCELLED' , 'EXPIRED')) DEFAULT 'PENDING_LOCK',
+        seat_lock_expires_at TEXT,
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (attendee_id) REFERENCES users(id),
+        FOREIGN KEY (tier_id) REFERENCES ticket_tiers(id)
 
-    )
+    );
 
     CREATE TABLE IF NOT EXISTS checkins(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         booking_id INTEGER UNIQUE NOT NULL,
-        qr_code INTEGER UNIQUE NOT NULL,
+        qr_code TEXT UNIQUE NOT NULL,
         verified_by_organizer_id INTEGER NOT NULL,
-        checked_in_at DATETIME DEFAULR CURRENT_TIMESTAMP,
-        FOREIGN KEY (booking_id) REFRENCE bookings(id)
-        FOREIGN KEY (verified_by_oraganizer_id) REFRENCE users(id)
+        checked_in_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (booking_id) REFERENCES bookings(id),
+        FOREIGN KEY (verified_by_organizer_id) REFERENCES users(id)
     )
 `
 
@@ -60,18 +64,11 @@ export const initSchema = (db)=>
     {
         return new Promise((resolve , reject)=>
             {
-                db.run("PRAGMA foreign_keys = ON;", (err)=>
+                db.exec(schema , (err)=>
                     {
-                        if(err) return reject(errr)
-
-                        db.exec(schema , (execError)=>
-                            {
-                                if (execErr) { console.error("❌ Failed to initialize database schema:", execErr.message); 
-                                    return reject(execErr); 
-                                } 
-                                console.log("📑 Database schema &amp; tables initialized successfully!"); 
-                                resolve(); 
-                            })
+                        if(err) return reject(err)
+                        console.log("📑 Database schema &amp; tables initialized successfully!");
+                        resolve();
                     })
             })
     }
